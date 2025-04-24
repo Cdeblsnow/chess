6194e6a2aa2f58e2f1713a29b475db7850cd08cb
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-24 13:25:08 -0400

    Add define_side test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,12 +2,30 @@ require_relative "../lib/game"
 require_relative "../lib/player"
 
 describe Game do
-  describe "player_init" do
+  describe "#player_init" do
     subject(:game_players) { described_class.new }
     it "creates two players" do
       list = game_players.instance_variable_get(:@player_list)
       game_players.player_init
       expect(list.length).to eq(2)
+    end
+  end
+
+  describe "#define_side" do
+    subject(:game_side) { described_class.new }
+    it "assign a side(black/white) to the the player" do
+      game_side.player_init
+      game_side.define_side
+      player = game_side.instance_variable_get(:@player_list)[0]
+      expect(player.show_player_side).to_not be(nil)
+    end
+
+    it "both players receive different sides" do
+      game_side.player_init
+      game_side.define_side
+      player1 = game_side.instance_variable_get(:@player_list)[0]
+      player2 = game_side.instance_variable_get(:@player_list)[1]
+      expect(player1.show_player_side).to_not eq(player2.show_player_side)
     end
   end
 end
