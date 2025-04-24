5b8f64706ed46cdba925fe96a2b045ed6d20e6a4
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-24 13:48:33 -0400

    Feat: now pawn pieces can be created

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,13 @@
+require_relative "piece/pawn"
 class Player
+  attr_reader :piece_set
+
+  COLUMNS = %w[a b c d e f g h].freeze
   def initialize(name)
     @name = name
     @points = 0
     @side = "" # make aleatory pick white/black
+    @piece_set = []
   end
 
   def add_point
@@ -15,5 +20,13 @@ class Player
 
   def show_player_side
     @side
+  end
+
+  def receive_set(set)
+    @piece_set << set
+  end
+
+  def create_pawn
+    8.times { |i| @piece_set << (Pawn.new(@side, [COLUMNS[i], 1])) }
   end
 end
