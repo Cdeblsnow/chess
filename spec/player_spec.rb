ffce5905416312bf8a4b79f3340a2ad674e7e9d8
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-24 14:40:11 -0400

    Add create_bishops test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,11 +10,11 @@ describe Player do
     end
   end
 
-  describe "#create_pawn" do
+  describe "#create_pawns" do
     subject(:player_pawn) { described_class.new("test") }
     it "creates eight pawns" do
       player_pawn.define_side("black")
-      player_pawn.create_pawn
+      player_pawn.create_pawns
       piece_set = player_pawn.instance_variable_get(:@piece_set)
       8.times { |i| expect(piece_set[i]).to be_a(Pawn) }
     end
@@ -37,6 +37,16 @@ describe Player do
       player_queen.create_queen
       piece_set = player_queen.instance_variable_get(:@piece_set)
       expect(piece_set[0]).to be_a(Queen)
+    end
+  end
+
+  describe "#create_bishops" do
+    subject(:player_bishops) { described_class.new("test") }
+    it "creates two bishops" do
+      player_bishops.define_side("black")
+      player_bishops.create_bishops
+      piece_set = player_bishops.instance_variable_get(:@piece_set)
+      piece_set.each { |piece| expect(piece).to be_a(Bishop) }
     end
   end
 end
