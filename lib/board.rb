ad6c5c4ea7252afaa8316fea30a1789eb83a962b
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-28 15:47:01 -0400

    Improve board printing method

--- lib/board.rb
+++ lib/board.rb
@@ -1,30 +1,40 @@
 class Board
   def initialize
-    @board_tiles = (1..8).to_h { |i| [i, Array.new(8) { [] }] }
+    @board_tiles = ("a".."h").to_h { |i| [i, Array.new(9) { [] }] }
   end
 
-  def display
-    current = tiles
+  def display(list)
+    current = tiles(list)
     puts " \n\n#{current}"
-    puts "  1   2    3   4   5   6   7   8\n\n"
+    puts "  a     b      c     d     e     f     g     h\n\n"
   end
 
-  def tiles
+  def tiles(list)
     position = +""
+    fill_board_tiles(list)
+    8.downto(1) do |index|
+      position << index.to_s
 
-    8.times do |index|
-      position << "#{index + 1}"
-
-      @board_tiles.each_value do
-        position << " \u{1FB7D} \u{1FB7E}"
+      @board_tiles.each_value do |value|
+        piece = value[index].to_s unless value[index].empty?
+        position << " \u{1FB7D} #{piece || ' '} \u{1FB7E}"
       end
       position << "\n"
+
       position << " "
       @board_tiles.each_value do
-        position << " \u{1FB7C} \u{1FB7F}"
+        position << " \u{1FB7C}   \u{1FB7F}"
       end
       position << "\n"
     end
     position
+  end
+
+  def fill_board_tiles(player_list)
+    set = player_list[0].piece_set
+    set.each do |piece|
+      puts piece.value
+      @board_tiles[piece.position[0]][piece.position[1]] = piece.value
+    end
   end
 end
