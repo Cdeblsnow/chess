1e65467892ac1734f4106e8fe5b155ccb04884f9
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-29 21:19:54 -0400

    Feat: generate move set for pawns

--- lib/piece/pawn.rb
+++ lib/piece/pawn.rb
@@ -9,8 +9,25 @@ class Pawn
       @value = "\u{2659}"
     end
     @position = position
+    @first_move = false
   end
 
-  def valid_move(move)
+  def possible_moves
+    moves = []
+    case @value
+    when "\u{265F}"
+      if @first_move == false
+        moves << [@position[0], @position[1] + 2]
+        @first_move == true
+      end
+      moves << [@position[0], @position[1] + 1]
+    when "\u{2659}"
+      if @first_move == false
+        moves << [@position[0], @position[1] - 2]
+        @first_move == true
+      end
+      moves << [@position[0], @position[1] - 1]
+    end
+    moves
   end
 end
