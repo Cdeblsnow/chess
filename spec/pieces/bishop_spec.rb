749c8281ef4dd6913e7df96698864ae086203404
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-05-12 19:06:14 -0400

    Add right ip and left up movement tests

---
+++ spec/pieces/bishop_spec.rb
@@ -0,0 +1,35 @@
+require_relative "../../lib/piece/bishop"
+
+describe Bishop do
+  describe "#right_upward_diagonal" do
+    subject(:bishop) { described_class.new("white", ["c", 1]) }
+    let(:board) { ("a".."h").to_h { |i| [i, Array.new(9) { [] }] } }
+
+    before do
+      bishop.update_column_index
+    end
+
+    it "return five positions from c1" do
+      bishop.right_upward_diagonal(board)
+      moves = bishop.instance_variable_get(:@moves)
+
+      expect(moves.length).to eq(5)
+    end
+  end
+
+  describe "#lef_upward_diagonal" do
+    subject(:bishop) { described_class.new("white", ["c", 1]) }
+    let(:board) { ("a".."h").to_h { |i| [i, Array.new(9) { [] }] } }
+
+    before do
+      bishop.update_column_index
+    end
+
+    it "return two positions from c1" do
+      bishop.lef_upward_diagonal(board)
+      moves = bishop.instance_variable_get(:@moves)
+
+      expect(moves.length).to eq(2)
+    end
+  end
+end
