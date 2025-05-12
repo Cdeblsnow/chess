e3f0a5d58f5d3d14a83f1f587e4679aa74a068ca
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-05-12 19:53:39 -0400

    Add left/right one tests

--- spec/pieces/king_spec.rb
+++ spec/pieces/king_spec.rb
@@ -20,4 +20,24 @@ describe King do
       expect(moves.length).to eq(0)
     end
   end
+
+  describe "#right_one" do
+    subject(:king) { described_class.new("white", ["e", 1]) }
+
+    it "return one position from e1" do
+      king.right_one
+      moves = king.instance_variable_get(:@moves)
+      expect(moves.length).to eq(1)
+    end
+  end
+
+  describe "#left_one" do
+    subject(:king) { described_class.new("white", ["e", 1]) }
+
+    it "return one position from e1" do
+      king.left_one
+      moves = king.instance_variable_get(:@moves)
+      expect(moves.length).to eq(1)
+    end
+  end
 end
