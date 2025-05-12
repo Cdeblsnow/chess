24307bf3bea2772391eb13e3c8e8cd6ccc767e71
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-05-12 19:47:21 -0400

    Add up/down one methods tests

---
+++ spec/pieces/king_spec.rb
@@ -0,0 +1,23 @@
+require_relative "../../lib/piece/king"
+
+describe King do
+  describe "#up_one" do
+    subject(:king) { described_class.new("white", ["e", 1]) }
+
+    it "return one position from e1" do
+      king.up_one
+      moves = king.instance_variable_get(:@moves)
+      expect(moves.length).to eq(1)
+    end
+  end
+
+  describe "#down_one" do
+    subject(:king) { described_class.new("white", ["e", 1]) }
+
+    it "return zero position from e1" do
+      king.down_one
+      moves = king.instance_variable_get(:@moves)
+      expect(moves.length).to eq(0)
+    end
+  end
+end
