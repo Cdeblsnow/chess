40bb87bec886a8f61b450644c710a4477011a035
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-05-15 22:08:29 -0400

    Add right up/down, down right/left movement tests

--- spec/pieces/knight_spec.rb
+++ spec/pieces/knight_spec.rb
@@ -22,4 +22,48 @@ describe Knight do
       expect(moves.flatten).to eq(["c", 3])
     end
   end
+
+  describe "#right_up" do
+    subject(:knight) { described_class.new("white", ["b", 1]) }
+
+    it "returns c3 position from b1" do
+      knight.right_up
+      moves = knight.instance_variable_get(:@moves)
+      p moves
+      expect(moves.flatten).to eq(["d", 2])
+    end
+  end
+
+  describe "#right_down" do
+    subject(:knight) { described_class.new("white", ["b", 1]) }
+
+    it "do not returns position from b1" do
+      knight.right_down
+      moves = knight.instance_variable_get(:@moves)
+      p moves
+      expect(moves.length).to eq(0)
+    end
+  end
+
+  describe "#down_right" do
+    subject(:knight) { described_class.new("white", ["b", 1]) }
+
+    it "do not returns position from b1" do
+      knight.down_right
+      moves = knight.instance_variable_get(:@moves)
+      p moves
+      expect(moves.length).to eq(0)
+    end
+  end
+
+  describe "#down_left" do
+    subject(:knight) { described_class.new("white", ["b", 1]) }
+
+    it "do not returns position from b1" do
+      knight.down_left
+      moves = knight.instance_variable_get(:@moves)
+      p moves
+      expect(moves.length).to eq(0)
+    end
+  end
 end
