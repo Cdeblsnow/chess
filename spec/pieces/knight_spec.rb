aa972a9fc4fdb33d2112a4e3fcbb1d8a400836c4
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-06-03 16:26:56 -0400

    Add extra tests

--- spec/pieces/knight_spec.rb
+++ spec/pieces/knight_spec.rb
@@ -7,7 +7,6 @@ describe Knight do
     it "returns a3 position from b1" do
       knight.up_left
       moves = knight.instance_variable_get(:@moves)
-      p moves
       expect(moves.flatten).to eq(["a", 3])
     end
   end
@@ -18,7 +17,6 @@ describe Knight do
     it "returns c3 position from b1" do
       knight.up_right
       moves = knight.instance_variable_get(:@moves)
-      p moves
       expect(moves.flatten).to eq(["c", 3])
     end
   end
@@ -29,7 +27,6 @@ describe Knight do
     it "returns c3 position from b1" do
       knight.right_up
       moves = knight.instance_variable_get(:@moves)
-      p moves
       expect(moves.flatten).to eq(["d", 2])
     end
   end
@@ -40,7 +37,6 @@ describe Knight do
     it "do not returns position from b1" do
       knight.right_down
       moves = knight.instance_variable_get(:@moves)
-      p moves
       expect(moves.length).to eq(0)
     end
   end
@@ -51,7 +47,6 @@ describe Knight do
     it "do not returns position from b1" do
       knight.down_right
       moves = knight.instance_variable_get(:@moves)
-      p moves
       expect(moves.length).to eq(0)
     end
   end
@@ -62,7 +57,6 @@ describe Knight do
     it "do not returns position from b1" do
       knight.down_left
       moves = knight.instance_variable_get(:@moves)
-      p moves
       expect(moves.length).to eq(0)
     end
   end
@@ -73,7 +67,6 @@ describe Knight do
     it "do not returns position from b1" do
       knight.left_down
       moves = knight.instance_variable_get(:@moves)
-      p moves
       expect(moves.length).to eq(0)
     end
   end
@@ -84,7 +77,86 @@ describe Knight do
     it "do not returns position from b1" do
       knight.left_up
       moves = knight.instance_variable_get(:@moves)
-      p moves
+      expect(moves.length).to eq(0)
+    end
+  end
+
+  describe "#up_left" do
+    subject(:knight) { described_class.new("black", ["g", 8]) }
+
+    it "returns no positions" do
+      knight.up_left
+      moves = knight.instance_variable_get(:@moves)
+      expect(moves.length).to eq(0)
+    end
+  end
+
+  describe "#up_right" do
+    subject(:knight) { described_class.new("black", ["g", 8]) }
+
+    it "returns no positions" do
+      knight.up_right
+      moves = knight.instance_variable_get(:@moves)
+      expect(moves.length).to eq(0)
+    end
+  end
+
+  describe "#right_up" do
+    subject(:knight) { described_class.new("black", ["g", 8]) }
+
+    it "returns no positions" do
+      knight.right_up
+      moves = knight.instance_variable_get(:@moves)
+      expect(moves.length).to eq(0)
+    end
+  end
+
+  describe "#right_down" do
+    subject(:knight) { described_class.new("black", ["g", 8]) }
+
+    it "returns no positions" do
+      knight.right_down
+      moves = knight.instance_variable_get(:@moves)
+      expect(moves.length).to eq(0)
+    end
+  end
+
+  describe "#down_right" do
+    subject(:knight) { described_class.new("black", ["g", 8]) }
+
+    it "returns h6 from g8" do
+      knight.down_right
+      moves = knight.instance_variable_get(:@moves)
+      expect(moves.flatten).to eq(["h", 6])
+    end
+  end
+
+  describe "#down_left" do
+    subject(:knight) { described_class.new("black", ["g", 8]) }
+
+    it "returns f6 from g8" do
+      knight.down_left
+      moves = knight.instance_variable_get(:@moves)
+      expect(moves.flatten).to eq(["f", 6])
+    end
+  end
+
+  describe "#left_down" do
+    subject(:knight) { described_class.new("black", ["g", 8]) }
+
+    it "returns e7from g8" do
+      knight.left_down
+      moves = knight.instance_variable_get(:@moves)
+      expect(moves.flatten).to eq(["e", 7])
+    end
+  end
+
+  describe "#left_up" do
+    subject(:knight) { described_class.new("black", ["g", 8]) }
+
+    it "do not returns position from b1" do
+      knight.left_up
+      moves = knight.instance_variable_get(:@moves)
       expect(moves.length).to eq(0)
     end
   end
